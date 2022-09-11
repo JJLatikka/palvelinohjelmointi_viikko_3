bbe89860081cfb08769b98faf6539996634cafac
Jussi_Latikka <jussilatikka@gmail.com>
2022-09-11 09:30:41 +0300

    aloitus

---
+++ src/main/java/sprbootjdbc/SprBootJdbcApplication.java
@@ -0,0 +1,13 @@
+package sprbootjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprBootJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprBootJdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sprbootjdbc/SprBootJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package sprbootjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprBootJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
